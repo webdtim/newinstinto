dd1a73879a4bccf2925426a3b3ad1e416567e047
Timur <timur@DESKTOP-P8A7R2S.localdomain>
2021-01-07 00:59:21 +0300

    Подправил в шапке каталог и поиск. Создал футер. Подправил главную.

--- src/blocks/drop-field/drop-field.js
+++ src/blocks/drop-field/drop-field.js
@@ -1,27 +1,25 @@
-/* global document */
-
-const ready = require('../../js/utils/documentReady.js');
-
-ready(function(){
-
   const catalogFilter = document.querySelector('#catalogFilter');
   const catalogDropFilterButton = document.querySelector('#catalogDropFilterButton');
 
   function selectCheckbox(e) {
 
     if (!e.target.classList.contains('drop-field__input')) return;
+    
     if (!e.target.checked) {
       e.target.closest('.drop-field__item').classList.remove('drop-field__item--active');
     } else {
       e.target.closest('.drop-field__item').classList.add('drop-field__item--active');
     }
   }
+
   function openCheckboxList(e) {
 
     if (!e.target.classList.contains('drop-field__title')) return;
+    
     if (!e.target.closest('.drop-field__inner')) return;
     e.target.closest('.drop-field__inner').classList.toggle('drop-field__inner--open');
   }
+
   function dropField() {
 
     catalogFilter.classList.toggle('close');
@@ -32,5 +30,5 @@ ready(function(){
     catalogFilter.addEventListener('change', selectCheckbox);
     catalogFilter.addEventListener('click', openCheckboxList);
   }
+
   if (catalogDropFilterButton) catalogDropFilterButton.addEventListener('click', dropField);
-});
