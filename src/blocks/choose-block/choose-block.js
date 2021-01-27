c62a261ffc56e6dd3a87c10358bbcecc7f96ec99
webdtim <47994950+webdtim@users.noreply.github.com>
2021-01-27 18:50:30 +0300

    Подправил выпадающие категории, стилизовал выбор доставки, подправил сетку в корзине(для улучшения дапатива)

--- src/blocks/choose-block/choose-block.js
+++ src/blocks/choose-block/choose-block.js
@@ -1,7 +1,30 @@
 /* global document */
 
-// const ready = require('../../js/utils/documentReady.js');
+const ready = require('../../js/utils/documentReady.js');
 
-// ready(function(){
-//   
-// });
+ready(function(){
+
+  const chooseBlockWrap = document.querySelector('.choose-block__wrap')
+
+  let chooseCheckbox = function(e) {
+
+    if (!e.target.closest('.choose-block')) return
+
+    const chooseBlock = e.target.closest('.choose-block'),
+          checkbox = chooseBlock.querySelector('input')
+
+    if (e.target.type == 'checkbox') {
+      checkbox.checked = !checkbox.checked
+      return
+    }
+
+    if (!chooseBlock) return
+    checkbox.checked = !checkbox.checked
+    chooseBlock.classList.toggle('choose-block--active')
+  }
+
+  if (chooseBlockWrap){
+    chooseBlockWrap.addEventListener('click', chooseCheckbox)
+  }
+
+});
